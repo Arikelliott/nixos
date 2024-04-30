a421bc75e3dfa82d165b8a8d1a8005a3d9e15dfe
Arikelliott <20728146+Arikelliott@users.noreply.github.com>
2024-04-30 00:38:07 -0400

    First full NixOS config build. Still needs small tweaks.

--- configuration.nix
+++ configuration.nix
@@ -1,98 +1,379 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+# My NixOS Config
+# Author: Arik Elliott
 
-{ config, lib, pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
+
+# -------- IMPORTS --------
+
+# - Import Hardware Scan -
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
 
+# - Other Imports -
 
 
-  # -- BOOTLOADER --
-  # Notes: Gonna try systemd-boot for the moment, let's see what it's like.
-  # Use the systemd-boot EFI boot loader. 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+
+# -------- BOOTLOADER AND PARTITIONING --------
+
+# - Setup Bootloader -
+  boot.loader = {
+	  efi = {
+	    canTouchEfiVariables = true; # Whether the installation process is allowed to modify EFI boot variables.
+	  };
+	  grub = { # Enable GRUB as bootloader.
+	     enable = true;
+	     efiSupport = true;
+	     device = "nodev";
+       useOSProber = true;
+	  };
+  };
+
+# - Mount Partitions -
+  fileSystems."/home" = { # Mount home partition.
+    device = "/dev/nvme1n1p4";
+    fsType = "ext4";
+    options = ["defaults"]; # Mount options (optional). NOTE: Use square brackets around string.
+  };
 
 
-  # -- STUFF --
 
+# -------- USER ACCOUNTS --------
+  # Define your user accounts. Don't forget to set a password with ‘passwd’!
+
+  users.users.arik = {
+     isNormalUser = true;
+     initialPassword = "pw123";
+     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user using "wheel" group.
+   };
+
+
+
+# -------- BASIC SYSTEM CONFIGURATION --------
+
+# - Networking -
   networking.hostName = "silvana"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  networking.networkmanager.enable = true;  # Enable NetworkManager
+  networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain"; # Configure network proxy if necessary
 
-  # Set your time zone.
-  time.timeZone = "America/Detroit";
+# - System Time -
+  time.timeZone = "America/Detroit"; # Set your time zone.
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+# - Keyboard and Localization -
+  services.xserver.layout = "us"; # Configure keymap in X11.
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-  console = {
-    font = "Lat2-Terminus16";
-  #  keyMap = "us";
-    useXkbConfig = true; # use xkb.options in tty.
-  };
-
-  
-  # -- DISPLAY MANAGER --
-  
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.displayManager.sddm.autoNumlock = true;
-  # services.xserver.displayManager.sddm.theme = sddm-sugar-light; # enable this later when you have the theme ready.
-  
-
-
-  
-
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.arik= {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-  #  packages = with pkgs; [
-  #    firefox
-  #    tree
-  #  ];
-  };
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    gnome
-    firefox
-    vscodium
-    zsh
-    stow
-    kitty
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    wget
-    curl
-    git
+# - Printing -
+  services.printing.enable = true; # Enable CUPS to print documents.
+  services.printing.drivers = with pkgs; [
+    gutenprint
+    hplip
+    splix
   ];
 
+# - Bluetooth -
+  hardware.bluetooth.enable = true; # enables support for Bluetooth
+  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
+
+# - Sound -
+  # sound.enable = true; # Enable default ALSA sound.
+  security.rtkit.enable = true; # Enables RealtimeKit service, which handles scheduling?
+  hardware.pulseaudio.enable = true;
+
+# - Mouse Input -
+  # services.xserver.libinput.enable = true; # Enable touchpad support (enabled default in most desktopManager).
+
+# - Drawing Tablets -
+  hardware.opentabletdriver.enable = true; # Enable OpenTabletDriver.
+
+# - SSH -
+  services.openssh.enable = true; # Enable the OpenSSH daemon.
+
+# - Firewall -
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  networking.firewall.enable = false;
+
+
+
+# -------- DESKTOP ENVIRONMENT CONFIGURATION --------
+
+# - Xserver -
+  services.xserver.enable = true; # Enable X11 windowing system.
+
+# - Display Manager -
+  services.xserver.displayManager = {
+    # gdm.enable = true; # Enable GNOME Display Manager.
+    # lightdm.enable = true; # Enable LightDM.
+    sddm = { # Settings for SDDM display manager.
+      enable = true; # Enable SDDM.
+      # wayland.enable = true; # Enable experimental Wayland support.
+      # theme = ""; # Theme to use.
+      autoNumlock = true; # Enable num lock.
+    };
+    defaultSession = "hyprland"; # Set pre-selected session when starting display manager.
+  };
+
+# - Desktop Environment List: - 
+  # - GNOME # --- Currently disabled!
+  services.xserver.desktopManager.gnome.enable = true; # Enable GNOME.
+  # environment.gnome.excludePackages = (with pkgs; [ # GNOME packages to exclude.
+  #   gnome-photos
+  #   gnome-tour
+  # ]) ++ (with pkgs.gnome; [
+  #   cheese # webcam tool
+  #   gnome-music
+  #   gnome-terminal
+  #   gedit # text editor
+  #   epiphany # web browser
+  #   geary # email reader
+  #   evince # document viewer
+  #   gnome-characters
+  #   totem # video player
+  #   tali # poker game
+  #   iagno # go game
+  #   hitori # sudoku game
+  #   atomix # puzzle game
+  # ]);
+
+  # - Hyprland
+  programs.hyprland.enable = true; # Enable Hyprland.
+
+  # - BSPWM
+  # services.xserver.windowManager.bspwm.enable = true; # Enable BSPWM
+
+# - Policy Kit -
+  security.polkit.enable = true;
+
+# - Disable xterm -
+  services.xserver.desktopManager.xterm.enable = false;
+
+
+
+# -------- SYSTEM SERVICES --------
+# NOTE: NixOS will install the required packagaes to make a service work even if
+# the package isn't specified in the packages list!
+# Find a list of services at: https://search.nixos.org/options?channel=23.11&show=jdk&size=50&sort=relevance&type=packages&query=jdk
+
+# - ckb-next -
+  hardware.ckb-next.enable = true; # Enable ckb-next daemon.
+
+# - OpenRGB -
+  services.hardware.openrgb.enable = true; # Enable OpenRGB daemon and other options.
+  services.hardware.openrgb.motherboard = "amd"; # Ensure it's using AMD modules (should be automatically though).
+
+# - APC UPS Daemon -
+  services.apcupsd.enable = true;
+
+# - Docker -
+  virtualisation.docker.enable = true; # Enable docker.
+  users.extraGroups.docker.members = [ "arik" ]; # List user accounts with Docker access.
+  # virtualisation.docker.rootless = { # Make docker run in a rootless mode for security reasons.
+  #   enable = true;
+  #   setSocketVariable = true;
+  # };
+
+# - Flatpak -
+  services.flatpak.enable = true; # Enable Flatpak.
+
+# - Spotify -
+  services.spotifyd.enable = true; # Enable Spotify daemon.
+
+# - Steam -
+  programs.steam = {
+  enable = true;
+  remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+  dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+};
+
+# - git -
+  programs.git = {
+    enable = true; # Enable git
+    # userName  = "Arikelliott";
+    # userEmail = "arikelliott@protonmail.com";
+    config.init.defaultBranch = "main";
+    # };
+  };
+
+
+
+# -------- SYSTEM PACKAGES --------
+# Find a list of packages at: https://search.nixos.org/packages?channel=23.11&show=jdk&size=50&sort=relevance&type=packages&query=jdk
+
+# - Enable non-free software packages -
+  nixpkgs.config.allowUnfreePredicate = _: true;
+
+# - Insecure Package List -
+  nixpkgs.config = {
+    allowInsecure = true;
+    permittedInsecurePackages = [
+      "electron-25.9.0"
+    ];
+  };
+
+
+# - System Package List -
+  environment.systemPackages = with pkgs; [
+
+# Main CLI Tools:
+    btop # Fancy top-like process viewer.
+    gotop # Fancy top-like process viewer.
+    htop # Top-like process viewer with a few more features.
+    wget # Webpage downloader utility thing.
+    tree # Shows contents of directories recursively.
+    ranger # TUI file manager.
+    # yazi # TUI file manager, maybe more feature-rich than Ranger.
+    # zoxide # "Smart" cd replacement.
+    tldr # Summarizes common commands for CLI utilities.
+    lsof # Lists open files and the corresponding processes.
+    screen # Lets you have detachable shell sessions.
+    fd # A simple, fast and user-friendly alternative to find.
+    feh # A light-weight image viewer.
+    figlet # Generate ASCII word art.
+    gallery-dl # Download images galleries from image hosting sites.
+    yt-dlp # Youtube/video downloader, youtube-dl fork.
+    ytfzf # Find, download, and watch Youtube videos from the terminal.
+    # gifsicle # CLI tool for editing and making GIFs.
+    glow # Utility for rendering Markdown in the terminal.
+    lynx # CLI web browser.
+    # mlocate # Index and search utility.
+    neofetch # System info fetcher and snowflake displayer.
+    nettools # Network control tools.
+    nmap # Network discovery tool.
+    netcat-openbsd # Network swiss-army knife.
+    traceroute # IP packet route tracer.
+    radeontop # AMDGPU utilization tool.
+    # lzop # File compression tool.
+    p7zip # 7zip implementation.
+    unrar # RAR archive utility.
+    unzip # .zip file utility.
+    zip # .zip file utility.
+    pywal # Color scheme generator/switcher.
+    rclone # CLI syncing tool.
+    ripgrep # Enhanced alternative to grep, I think.
+    # ripgrep-all # ripgrep with more features.
+    # socat # Some fancy data redirection tool.
+    stow # Symlink/dotfiles manager.
+    zsh # Z Shell.
+    zsh-powerlevel10k # Powerlevel10k theme for zsh.
+
+# Admin/Desktop Tools:
+    kitty # My preferred terminal program.
+    baobab # Like WinDirStat. GTK graphical file usage viewer.
+    gparted # GUI disk partitioning tool.
+    font-manager # Font viewing app.
+    gucharmap # GNOME character map viewer.
+    galculator # GTK Calculator.
+    grsync # GUI rsync wrapper.
+    kdiff3 # Diff and merge up to 3 files or directories at once.
+    meld # GTK diff and merge tool.
+    # lact # AMDGPU GUI software. *****
+    dunst # Standalone notificatin manager for WMs.
+    grim # Wayland screenshot utility.
+    grimblast # Replacing hyprshot?
+    scrot # Another screen capture utility.
+    slurp # Wayland region selector utility.
+    wf-recorder # Wayland screen recorder.
+    hyprpicker # Wayland color picker.
+    swww # Fancy Wayland wallpaper setter and daemon.
+    waypaper # GUI frontend for swww.
+    loupe # GUI Image Viewer.
+    # lxappearance # GTK application theme and font configurer.
+    nwg-look # Wayland alternative to lxappearance.
+    cinnamon.nemo-with-extensions # Nemo file explorer
+    cinnamon.nemo-fileroller
+    # xfce.thunar # Thunar file explorer.
+    remmina # Remote desktop client.
+    rofi-wayland # Wayland fork of Rofi app launcher.
+    waybar # Wayland bar.
+    wireshark # Packet analyzer.
+    wlr-randr # Wayland xrandr clone.
+    wshowkeys # Shows keys being pressed (Wayland session only).
+
+# System Utilities:
+    cdrtools # CD, DVD, and Blu-Ray burning tools.
+    libisoburn # CD burning tool.
+    # mtools # Utilities to access MS-DOS disks.
+    # ntfs3g # NTFS filesystem driver.
+    usbutils # Tools for working with USB devices, such as lsusb.
+    smartmontools # Hard drive health monitoring tools.
+    ckb-next # Handles Corsair input devices/RGB/etc.
+    # exfatprogs # exFAT filesystem utilities.
+    foomatic-db-engine # A big set of utilities for printing.
+    ghostscript # Postscript interpreter for PDFs and stuff.
+    jdk # Java 19, main release of Java on NixOS. May include other Java versions too?
+    libsForQt5.polkit-kde-agent # KDE polkit agent.
+
+# Social:
+    discord # The popular chat app for lit gamers.
+    betterdiscordctl # Discord client modder.
+    irssi # Terminal-based IRC client.
+
+# Gaming:
+    prismlauncher # Minecraft instance manager.
+    mangohud # Game stat overlay software.
+
+# Productivity:
+    firefox
+    chromium
+    obsidian # Personal note-taking and thought organizing app.
+    # calibre # E-reader
+    # evince # All-purpose document viewer (PDFs, etc.).
+    cura # Slicer for 3D printing.
+    # freecad # Open-source CAD software.
+    # openscad # Programmatic CAD software.
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    neovim # Extensible vim fork.
+    vscodium # Fancy GUI IDE.
+    xfce.mousepad # basic text editor from XFCE.
+    hugo # Static website generator.
+
+# Media:
+    audacity # Audio editing suite.
+    lmms # Linux music production software.
+    libsForQt5.kdenlive # KDE's video editor.
+    # ft2-clone # Fasttracker II clone.
+    furnace # Chiptune tracker.
+    # goattracker # Commodore 64 tracker.
+    # klystrack # C64/Amiga/NES tracker.
+    blender-hip # HIP/ROCm version of Blender?
+    # blender # 3D Modeling software.
+    inkscape # Vector illustration program.
+    krita # Photo editing and painting program.
+    spotify # Want a break from the ads? Try Spotify Premium!
+    # calf # Open source audio plugins for musicians.
+    # easyeffects # PipeWire audio effects.
+    ffmpeg # Audio and video tool.
+    ffmpegthumbnailer # Generates thumbnails of images and video for file browsers, etc.
+    helvum # GTK Patchbay for PipeWire.
+    mpv # Media player.
+    obs-studio # Video recording software.
+    strawberry # Music player app.
+
+# Fonts and Themes:
+    noto-fonts # Google's universal font.
+    font-awesome # scalable vector icon font.
+    nerdfonts # Giant nerd font package.
+    jetbrains-mono
+    corefonts # Microsoft's TTF core fonts for the web.
+    materia-theme # Materia GTK theme.
+    material-icons # Google's Material icons.
+    papirus-icon-theme
+    
+  ];
+
+# - User Package Lists
+
+  # - Arik's packages
+  users.users.arik.packages = with pkgs; [ # Install these packages for this single user.
+    # example-package
+  ];
+
+# - Other -
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   programs.mtr.enable = true;
@@ -101,39 +382,32 @@
     enableSSHSupport = true;
   };
 
-  # List services that you want to enable:
 
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+
+
+
+
+
+
+
+
+
+
+# -------- SYSTEM STATE --------
 
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
   # accidentally delete configuration.nix.
   # system.copySystemConfiguration = true;
 
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "23.11";
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It's perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
 
 }
 
