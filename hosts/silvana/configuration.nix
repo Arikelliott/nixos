6299bfc00b7c87d3509533eaa9f6177b81b4816d
Arikelliott <20728146+Arikelliott@users.noreply.github.com>
2024-06-05 15:40:19 -0400

    Rearranging files for multiple hosts.

--- hosts/silvana/configuration.nix
+++ hosts/silvana/configuration.nix
@@ -157,16 +157,18 @@
   services.xserver.enable = true; # Enable X11 windowing system.
 
 # - Display Manager -
-  services.displayManager = {
-    # gdm.enable = true; # Enable GNOME Display Manager.
-    # lightdm.enable = true; # Enable LightDM.
-    sddm = { # Settings for SDDM display manager.
-      enable = true; # Enable SDDM.
-      # wayland.enable = true; # Enable experimental Wayland support.
-      # theme = "sddm-chili-theme"; # Theme to use.
-      autoNumlock = true; # Enable num lock.
-    };
-    defaultSession = "hyprland"; # Set pre-selected session when starting display manager.
+  # services.displayManager = {
+    # sddm = { # Settings for SDDM display manager.
+    #   enable = true; # Enable SDDM.
+    #   # wayland.enable = true; # Enable experimental Wayland support.
+    #   # theme = "sddm-chili-theme"; # Theme to use.
+    #   autoNumlock = true; # Enable num lock.
+    # };
+  #   defaultSession = "hyprland"; # Set pre-selected session when starting display manager.
+  # };
+
+  services.xserver.displayManager = { # Disable LightDM default when no other display manager is enabled.
+    lightdm.enable = false;
   };
 
 # - Desktop Environment List: - 
