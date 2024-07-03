35c949dac51e32a8a5e45971c8eb61cace2b892c
Arikelliott <20728146+Arikelliott@users.noreply.github.com>
2024-07-03 00:05:23 -0400

    Very small nixit script tweak

--- nixit.sh
+++ nixit.sh
@@ -1,11 +1,11 @@
 #! /usr/bin/env bash
 
-# A script to copy configuration.nix from its user-owned repo to /etc/nixos and run nixos-rebuild with it.
+# A script to install configuration.nix from the current 
 # Must be run from the nixos repo directory's top folder.
 
 # Get input from user
 # $1 - Get the name of the host configuration to be used (e.g. hosts/silvana)
 # $2 - Get the kind of rebuild you'd like to do (e.g. switch, boot, etc.)
 
-# sudo cp ./hosts/$1/configuration.nix /etc/nixos/
+
 sudo nixos-rebuild -I ./hosts/$1/configuration.nix $2
