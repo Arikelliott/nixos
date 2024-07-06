c766619ff28586dce608f5d1bda07129c3e8a76d
Arikelliott <20728146+Arikelliott@users.noreply.github.com>
2024-07-06 00:34:11 -0400

    Set my hardware-config back to default. It's a long story, had to sorta nuke my system. Good thing NixOS is so cool

--- hosts/silvana/hardware-configuration.nix
+++ hosts/silvana/hardware-configuration.nix
@@ -8,18 +8,18 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/f0514629-3c6b-4ae0-a401-15534e2d52cd";
+    { device = "/dev/disk/by-uuid/14ecf3b7-156a-4fec-8c34-07f60f01ef63";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/8A49-0DE1";
+    { device = "/dev/disk/by-uuid/5362-3D2E";
       fsType = "vfat";
       options = [ "fmask=0022" "dmask=0022" ];
     };
@@ -32,8 +32,6 @@
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp8s0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.veth3c26568.useDHCP = lib.mkDefault true;
-  # networking.interfaces.veth5192d9a.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
