879243cac5fffdb7dee1336254320c295f6046fa
Arikelliott <20728146+Arikelliott@users.noreply.github.com>
2024-07-22 07:53:43 -0400

    Finished configuring Ollama and Open WebUI

--- modules/software/docker/ollama.nix
+++ modules/software/docker/ollama.nix
@@ -19,11 +19,25 @@
                     "--device=/dev/dri"
                 ];
             };
-            # ollama-webui = { # Run WebUI for Ollama.
-            #   hostname = "open-webui";
-            #   image = "ghcr.io/open-webui/open-webui:main";
-            # };
+            open-webui = { # Run WebUI for Ollama.
+                hostname = "open-webui";
+                image = "ghcr.io/open-webui/open-webui:main";
+                volumes = [
+                    "open-webui:/app/backend/data"
+                ];
+                ports = [
+                    "3000:8080"
+                ];
+                extraOptions = [
+                    "--add-host=host.docker.internal:host-gateway"
+                ];
+            };
         };
     };
 
 }
+
+
+# docker run -d -p 3000:8080 --add-host=host.docker.internal:host-gateway -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:main
+
+# docker run -d --device /dev/kfd --device /dev/dri -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama:rocm
