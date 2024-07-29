b0e1f98ddd28d90f71d51dc49d766d403a08bf86
Arikelliott <20728146+Arikelliott@users.noreply.github.com>
2024-07-29 13:40:06 -0400

    First version of basic flake

--- flake.nix
+++ flake.nix
@@ -1,15 +1,21 @@
 {
-  description = "A very basic flake";
+	description = "A very basic flake";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
+	inputs = {
+		nixpkgs = {
+		url = "github:nixos/nixpkgs/nixos-24.05";
+		};
+	};
 
-  outputs = { self, nixpkgs }: {
+	outputs = { self, nixpkgs }: {
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
-  };
+		nixosConfigurations = {
+			silvana = nixpkgs.lib.nixosSystem {
+				system = "x86_64-linux";
+				modules = [
+					./hosts/silvana
+				];
+			};
+		};
+	};
 }
