6611fe260ee0baa808069bb5745a001e6e8c8f04
Arikelliott <20728146+Arikelliott@users.noreply.github.com>
2024-07-29 17:43:47 -0400

    Set up flake with my various hosts

--- flake.nix
+++ flake.nix
@@ -10,6 +10,34 @@
 	outputs = { self, nixpkgs }: {
 
 		nixosConfigurations = {
+			agrew = nixpkgs.lib.nixosSystem {
+				system = "x86_64-linux";
+				modules = [
+					./hosts/agrew
+				];
+			};
+
+			forrester = nixpkgs.lib.nixosSystem {
+				system = "x86_64-linux";
+				modules = [
+					./hosts/forrester
+				];
+			};
+
+			savannah = nixpkgs.lib.nixosSystem {
+				system = "x86_64-linux";
+				modules = [
+					./hosts/savannah
+				];
+			};
+
+			shetland = nixpkgs.lib.nixosSystem {
+				system = "x86_64-linux";
+				modules = [
+					./hosts/shetland
+				];
+			};
+
 			silvana = nixpkgs.lib.nixosSystem {
 				system = "x86_64-linux";
 				modules = [
