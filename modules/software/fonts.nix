ca085ed9dd90c131c4cfc668a864b7fac95f2ad9
Arikelliott <20728146+Arikelliott@users.noreply.github.com>
2024-07-31 19:29:32 -0400

    Added a few extra font packages

--- modules/software/fonts.nix
+++ modules/software/fonts.nix
@@ -8,7 +8,10 @@ fonts.packages = with pkgs; [
 	font-awesome # scalable vector icon font.
 	nerdfonts # Giant nerd font package.
 	jetbrains-mono
-	# corefonts # Microsoft's TTF core fonts for the web.
+	corefonts # Microsoft's TTF core fonts for the web.
+	helvetica-neue-lt-std # Helvetica Neue Standard
+	aileron # Clean sans serif font, sorta similar to Helvetica
+
 	materia-theme # Materia GTK theme.
 	material-icons # Google's Material icons.
 	papirus-icon-theme
