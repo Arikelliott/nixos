70c4c8b75bbad74f5224285c9202cb832f41dffa
Arikelliott <20728146+Arikelliott@users.noreply.github.com>
2024-08-01 21:04:04 -0400

    Small changes to get Home Manager working properly, including switching to NixOS Unstable channel

--- flake.nix
+++ flake.nix
@@ -2,14 +2,14 @@
 	description = "A very basic flake";
 
 	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 		home-manager = {
 			url = "github:nix-community/home-manager";
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
 	};
 
-	outputs = { self, nixpkgs, home-manager, ... }@inputs: # Figure out what the "inputs@" bit does. Seems to work the same as without?
+	outputs = inputs @ { self, nixpkgs, home-manager, ... }: # Figure out what the "inputs@" bit does. Seems to work the same as without?
 	{
 		nixosConfigurations = {
 			agrew = nixpkgs.lib.nixosSystem {
@@ -49,6 +49,7 @@
 				specialArgs = { inherit inputs; };
 				modules = [
 					./hosts/silvana
+					home-manager.nixosModules.home-manager
 				];
 			};
 		};
