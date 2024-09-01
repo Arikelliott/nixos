b21f217fb0e55243dd90e3bc68d9daf2d4b2f389
Arikelliott <20728146+Arikelliott@users.noreply.github.com>
2024-09-01 16:15:55 -0400

    typo

--- hosts/silvana/home.nix
+++ hosts/silvana/home.nix
@@ -5,7 +5,7 @@
 	# manage.
 	home.username = "arik";
 	home.homeDirectory = "/home/arik";
-	hostname = hostname;
+	# hostname = hostname;
 
 	# This value determines the Home Manager release that your configuration is
 	# compatible with. This helps avoid breakage when a new Home Manager release
