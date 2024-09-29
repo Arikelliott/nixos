13e085b82ed804202fedc1eb545b2f9e20c0d87a
Arikelliott <20728146+Arikelliott@users.noreply.github.com>
2024-09-28 20:59:37 -0400

    Updating flake and enabling Cosmic desktop

---
+++ modules/software/desktop-environments/cosmic.nix
@@ -0,0 +1,67 @@
+{ config, pkgs, ... }:
+
+{
+
+    imports = [
+
+		# - Import configs exclusive to this host
+		    ./icons.nix
+        ./themes.nix
+
+    ];
+
+    # - Xserver -
+    services.xserver.enable = true; # Enable X11 windowing system.
+
+    # - Display Manager -
+    # services.displayManager = {
+        # sddm = { # Settings for SDDM display manager.
+        #   enable = true; # Enable SDDM.
+        #   # wayland.enable = true; # Enable experimental Wayland support.
+        #   # theme = "sddm-chili-theme"; # Theme to use.
+        #   autoNumlock = true; # Enable num lock.
+        # };
+    #   defaultSession = "hyprland"; # Set pre-selected session when starting display manager.
+    # };
+
+    services.displayManager = { # Disable LightDM default when no other display manager is enabled.
+        cosmic.greeter.enable = false;
+    };
+
+    # - Hyprland
+    services.desktopManager.cosmic.enable = true; # Enable Hyprland.
+
+
+
+    environment.systemPackages = with pkgs; [
+		xorg.xhost # Server access control program for X. (May be needed for Gparted?)
+		gparted # GUI disk partitioning tool.
+		kitty # My preferred terminal program.
+		# dunst # Standalone notificatin manager for WMs.
+		# grim # Wayland screenshot utility.
+		# hyprshot # hyprland screenshot utility.
+		# scrot # Another screen capture utility.
+		# slurp # Wayland region selector utility.
+		# wf-recorder # Wayland screen recorder.
+		# hyprpicker # Wayland color picker.
+		swww # Fancy Wayland wallpaper setter and daemon.
+		waypaper # GUI frontend for swww.
+		loupe # GUI Image Viewer.
+        # lxappearance # Doesn't work on Wayland at all
+		nwg-look # Wayland alternative to lxappearance.
+		nemo-with-extensions # Nemo file explorer
+		nemo-fileroller
+		# rofi-wayland # Wayland fork of Rofi app launcher.
+		# waybar # Wayland bar.
+		wireshark # Packet analyzer.
+		wlr-randr # Wayland xrandr clone.
+		wshowkeys # Shows keys being pressed (Wayland session only).
+		# libsForQt5.polkit-kde-agent # KDE polkit agent.
+		lxqt.lxqt-policykit # LXQT polkit agent.
+		pwvucontrol # Pipewire volume control interface.
+		pywal # Color scheme generator/switcher.
+		stow # Symlink/dotfiles manager.
+		
+	];
+
+}
