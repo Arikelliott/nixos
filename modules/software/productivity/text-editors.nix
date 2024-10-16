95f3e53678936d07426de09a5e9ba430e4e5851a
Arikelliott <20728146+Arikelliott@users.noreply.github.com>
2024-10-16 17:26:15 -0400

    Trying out Zed

--- modules/software/productivity/text-editors.nix
+++ modules/software/productivity/text-editors.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
 
@@ -7,6 +7,7 @@ environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     neovim # Extensible vim fork.
     vscodium # Fancy GUI IDE.
+    zed-editor # New IDE trying to replace vscodium.
     xfce.mousepad # basic text editor from XFCE.
 
 ];
