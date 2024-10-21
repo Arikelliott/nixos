67037079e1d2f82f6ecb7afeb6bfdb379a85f704
Arikelliott <20728146+Arikelliott@users.noreply.github.com>
2024-10-21 08:13:27 -0400

    Getting zsh figured out

--- home-manager/zsh/default.nix
+++ home-manager/zsh/default.nix
@@ -3,19 +3,33 @@
 {
 	programs.zsh = {
 		enable = true;
+		initExtraFirst = ''
+            # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+            # Initialization code that may require console input (password prompts, [y/n]
+            # confirmations, etc.) must go above this block; everything else may go below.
+            if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
+            source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
+            fi
+
+            # Enable p10k theme
+            source ~/.config/powerlevel10k/powerlevel10k.zsh-theme
+
+            # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+            [[ ! -f ~/.config/.p10k.zsh ]] || source ~/.config/.p10k.zsh
+        '';
 	};
 
 	# Import p10k and theme.
 	home.file = {
-	 #   ".config/powerlevel10k" = {
-		# 	enable = true;
-		# 	source = ./powerlevel10k;
-		# 	# recursive = true;
-		# };
-		# ".config/p10k.zsh" = {
-		#     enable = true;
-		# 	source = ./.p10k.zsh;
-		# };
+	   ".config/powerlevel10k" = {
+			enable = true;
+			source = ./powerlevel10k;
+			recursive = true;
+		};
+		".config/p10k.zsh" = {
+		    enable = true;
+			source = ./.p10k.zsh;
+		};
 	};
 }
 
