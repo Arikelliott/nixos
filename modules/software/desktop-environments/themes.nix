596ef7f9a7818be3cccd363aa9d5fa6fc76ad386
Arikelliott <20728146+Arikelliott@users.noreply.github.com>
2024-10-22 09:13:31 -0400

    Adding gtk themes to test

--- modules/software/desktop-environments/themes.nix
+++ modules/software/desktop-environments/themes.nix
@@ -6,7 +6,28 @@
         materia-theme
         catppuccin
         catppuccin-gtk
-		
-	];
+	rose-pine-gtk-theme
+	whitesur-gtk-theme
+	tokyonight-gtk-theme
+	sierra-gtk-theme
+	lavanda-gtk-theme
+	paper-gtk-theme
+	qogir-gtk-theme
+	nightfox-gtk-theme
+	mojave-gtk-theme
+	matrix-gtk-theme
+	matcha-gtk-theme
+	lounge-gtk-theme
+	layan-gtk-theme
+	kanagawa-gtk-theme
+	gruvbox-gtk-theme
+	graphite-gtk-theme
+	fluent-gtk-theme
+	everforest-gtk-theme
+	deepin.deepin-gtk-theme
+	colloid-gtk-theme
+	andromeda-gtk-theme
+	adapta-gtk-theme
+    ];
 
 }
