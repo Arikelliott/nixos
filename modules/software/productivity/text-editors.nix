e9a74ff7d1e7d2331492eeb4fd0c2b96354d67bb
Arikelliott <20728146+Arikelliott@users.noreply.github.com>
2024-10-22 21:41:31 -0400

    disabling vscodium, all hail zed

--- modules/software/productivity/text-editors.nix
+++ modules/software/productivity/text-editors.nix
@@ -6,7 +6,7 @@ environment.systemPackages = with pkgs; [
 
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     neovim # Extensible vim fork.
-    vscodium # Fancy GUI IDE.
+    # vscodium # Fancy GUI IDE.
     zed-editor # New IDE trying to replace vscodium.
     xfce.mousepad # basic text editor from XFCE.
 
