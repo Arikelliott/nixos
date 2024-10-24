55ab5401b4de8578fe4721cb0d50111d29f8e943
Arikelliott <20728146+Arikelliott@users.noreply.github.com>
2024-10-24 11:48:29 -0400

    Tweaked nix rebuild scripts. Fixed descriptions and made them run from the current directory instead of from a hard-coded nix repo path

--- home-manager/nixtools/nixit.sh
+++ home-manager/nixtools/nixit.sh
@@ -1,13 +1,11 @@
 #! /usr/bin/env bash
 
-# A script to install configuration.nix from the current 
-# Must be run from the nixos repo directory's top folder.
+# A script to shorten the Nix Rebuild command.
+#
+# Must be run from the Nix repo directory's top folder and include a flake.
 
-# Get input from user
+# Input needed from user:
 # $1 - Get the name of the host configuration to be used (e.g. hosts/silvana)
 # $2 - Get the kind of rebuild you'd like to do (e.g. switch, boot, etc.)
 
-
-# sudo nixos-rebuild -I nixos-config=./hosts/$1 $2 --upgrade
-
-sudo nixos-rebuild $2 --flake "/home/arik/Documents/repos/nixos#$1"
+sudo nixos-rebuild $2 --flake "./#$1"
--- home-manager/nixtools/nixremote.sh
+++ home-manager/nixtools/nixremote.sh
@@ -1,13 +1,15 @@
 #! /usr/bin/env bash
 
-# A script to install configuration.nix from the current
-# Must be run from the nixos repo directory's top folder.
+# A script to shorten the Nix Rebuild command.
+# nixremote.sh will build a config locally and install to a remote machine.
+#
+# Must be run from the Nix repo directory's top folder and include a flake.
 
-# Get input from user
+# Input needed from user:
 # $1 - Get the name of the host configuration to be used (e.g. hosts/silvana)
 # $2 - Get the kind of rebuild you'd like to do (e.g. switch, boot, etc.)
 # $3 - Get the [hostname@ip-address] you're sending it to
 
 
 
-sudo nixos-rebuild $2 --flake "/home/arik/Documents/repos/nixos#$1" --target-host $3 --use-remote-sudo
+sudo nixos-rebuild $2 --flake "./#$1" --target-host $3 --use-remote-sudo
