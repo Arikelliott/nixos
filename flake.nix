c142907f20fd04a5f5270026040416162e713a85
Arikelliott <20728146+Arikelliott@users.noreply.github.com>
2024-10-24 11:54:08 -0400

    Fixed formatting for cosmic section of flake.nix

--- flake.nix
+++ flake.nix
@@ -65,15 +65,15 @@
 					./hosts/silvana
 					home-manager.nixosModules.home-manager
 
-					# Begin Cosmic-related stuff
-    					# {
-         #                    nix.settings = {
-         #                    substituters = [ "https://cosmic.cachix.org/" ];
-         #                    trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
-         #                    };
-         #                }
-    					# nixos-cosmic.nixosModules.default
-					# End Cosmic-related stuff
+            # Begin Cosmic-related stuff
+            # {
+            #     nix.settings = {
+            #         substituters = [ "https://cosmic.cachix.org/" ];
+            #         trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
+            #     };
+            # }
+            # nixos-cosmic.nixosModules.default
+            # End Cosmic-related stuff
 
 				];
 			};
