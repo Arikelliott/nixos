42c1938f4b300d328da138074bddcc20389bcfdc
Arikelliott <20728146+Arikelliott@users.noreply.github.com>
2024-11-12 11:27:09 -0500

    Small additions to Rofi setup

--- home-manager/rofi/default.nix
+++ home-manager/rofi/default.nix
@@ -5,12 +5,25 @@
         package = rofi-wayland;
         # cycle = true; # Something about cycling through results. Default is null, but true/false are also options.
 
+        # Settings to put in config.rasi, formatted Nix-style.
+        extraConfig = ''
+            lalala
+        '';
 
-	xdg.configFile.".config/rofi/theme.rasi" = {
-		enable = true;
-		text = ''
+    };
 
-		'';
-	};
+    home.file = {
+            ".config/rasi/theme.rasi" = {
+                enable = true;
+                source = ./theme.rasi;
+            };
+    };
+
+	# xdg.configFile.".config/rofi/theme.rasi" = {
+	# 	enable = true;
+	# 	text = ''
+
+	# 	'';
+	# };
 
 }
