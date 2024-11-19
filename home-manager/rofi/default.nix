26531b477cfb51dba56e2ce005544c30296bdccf
Arikelliott <20728146+Arikelliott@users.noreply.github.com>
2024-11-18 23:14:35 -0500

    Fixed rofi config so it works properly

--- home-manager/rofi/default.nix
+++ home-manager/rofi/default.nix
@@ -1,19 +1,20 @@
 {
 
     programs.rofi = {
-        enable = true;
-        package = rofi-wayland;
+        # enable = true;
+        # package = rofi-wayland;
         # cycle = true; # Something about cycling through results. Default is null, but true/false are also options.
 
         # Settings to put in config.rasi, formatted Nix-style.
-        extraConfig = ''
-            lalala
-        '';
-
+        # extraConfig = ''
+        #     lalala
+        # '';
     };
 
+
+
     home.file = {
-            ".config/rasi/config.rasi" = {
+            ".config/rofi/config.rasi" = {
                 enable = true;
                 source = ./config.rasi;
             };
