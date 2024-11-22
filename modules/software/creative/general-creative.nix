163db942065144bc27bd552ed6777aaef75d4301
Arikelliott <20728146+Arikelliott@users.noreply.github.com>
2024-11-21 19:36:26 -0500

    Added several new creative apps

--- modules/software/creative/general-creative.nix
+++ modules/software/creative/general-creative.nix
@@ -13,10 +13,13 @@ environment.systemPackages = with pkgs; [
     # klystrack # C64/Amiga/NES tracker.
     blender-hip # HIP/ROCm version of Blender?
     # blender # 3D Modeling software.
+    gimp # Raster editing program.
     inkscape # Vector illustration program.
     krita # Photo editing and painting program.
     # calf # Open source audio plugins for musicians.
     # easyeffects # PipeWire audio effects.
+    davinci-resolve # Video editing program.
+    # davinci-resolve-studio # Paid version of DaVinci. Doesn't include H264/265 support, convert with ffmpeg. https://wiki.nixos.org/wiki/DaVinci_Resolve#Cannot_import_video_file
     ffmpeg # Audio and video tool.
     ffmpegthumbnailer # Generates thumbnails of images and video for file browsers, etc.
     helvum # GTK Patchbay for PipeWire.
@@ -24,8 +27,11 @@ environment.systemPackages = with pkgs; [
     obs-studio # Video recording software.
     strawberry # Music player app.
     # cura # Slicer for 3D printing.
-    # freecad # Open-source CAD software.
+    freecad-wayland # Open-source CAD software.
     # openscad # Programmatic CAD software.
+    love # Love2D, a 2D game engine that used Lua scripting.
+    libresprite # Pixel art software, FOSS fork of aseprite.
+    # aseprite # Pixel art software.
 
 ];
 
