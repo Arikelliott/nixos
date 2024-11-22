5014cdc2e09f13ccb7af3aaf1a06f22ee86afefb
Arikelliott <20728146+Arikelliott@users.noreply.github.com>
2024-11-22 10:59:25 -0500

    Removed deprecated icon package

--- modules/software/desktop-environments/icons.nix
+++ modules/software/desktop-environments/icons.nix
@@ -6,14 +6,13 @@
         papirus-folders
         papirus-icon-theme
         papirus-maia-icon-theme
-        luna-icons
         whitesur-icon-theme
         vimix-icon-theme
         rose-pine-icon-theme
         material-icons
         material-symbols
         adwaita-icon-theme
-		
+
 	];
 
 }
