746e86bedf7ef034d28ba4559c40f4542da0b706
Arikelliott <20728146+Arikelliott@users.noreply.github.com>
2024-11-22 23:14:45 -0500

    Reverting previous commit, I think

--- modules/software/productivity/general-productivity.nix
+++ modules/software/productivity/general-productivity.nix
@@ -1,8 +1,8 @@
-{ pkgs, pkgs-stable, ... }:
+{ config, pkgs, ... }:
 
 {
 
-environment.systemPackages = (with pkgs; [
+environment.systemPackages = with pkgs; [
 
     chromium
     obsidian # Personal note-taking and thought organizing app.
@@ -10,17 +10,10 @@ environment.systemPackages = (with pkgs; [
     # evince # All-purpose document viewer (PDFs, etc.).
     hugo # Static website generator.
     pandoc # General filetype converter (eg. md to html).
+    libreoffice-fresh # LibreOffice
     teams-for-linux # Microsoft Teams
     zoom-us # Video call software
 
-])
-
-++
-
-(with pkgs-stable; [
-
-    libreoffice-fresh  # LibreOffice
-
-]);
+];
 
 }
