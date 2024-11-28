da71e88836ee69fafbe38171b2b0a1e8de6305bb
Arikelliott <20728146+Arikelliott@users.noreply.github.com>
2024-11-28 16:20:54 -0500

    Added amdvlk and vulkan utils

--- modules/software/amdgpu-utils.nix
+++ modules/software/amdgpu-utils.nix
@@ -2,11 +2,15 @@
 
 {
 
-	environment.systemPackages = with pkgs; [
-	
+	hardware.amdgpu.amdvlk.enable = true;
+
+    environment.systemPackages = with pkgs; [
+
 	radeontop # AMDGPU utilization tool.
     amdgpu_top # AMD GPU CLI monitoring tool.
     lact # AMDGPU GUI software. *****
+    vulkan-tools # Utils for Vulkan
+    vulkan-cts # Vulkan conformance tests I guess
 
 	];
 
