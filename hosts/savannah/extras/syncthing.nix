9c79b83ef9ac21ae13a7c409d6ca1283ff7dfcbb
Arikelliott <20728146+Arikelliott@users.noreply.github.com>
2024-12-01 15:23:09 -0500

    Updates to Syncthing configs

--- hosts/savannah/extras/syncthing.nix
+++ hosts/savannah/extras/syncthing.nix
@@ -4,10 +4,21 @@
 
   services.syncthing = {
     enable = true;
+    group = "users";
+    user = "arik";
+    dataDir = "/home/arik/Sync";
+    configDir = "/home/arik/.config/syncthing";
   };
 
   environment.systemPackages = with pkgs; [
     syncthing
   ];
+
+    # Syncthing ports: 8384 for remote access to GUI
+    # 22000 TCP and/or UDP for sync traffic
+    # 21027/UDP for discovery
+    # source: https://docs.syncthing.net/users/firewall.html
+    networking.firewall.allowedTCPPorts = [ 8384 22000 ];
+    networking.firewall.allowedUDPPorts = [ 22000 21027 ];
 
 }
