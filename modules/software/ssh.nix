04fbd2ad118a7c08ab77981d4fabf63b583cc572
Arikelliott <20728146+Arikelliott@users.noreply.github.com>
2024-12-06 14:31:13 -0500

    Enable ssh-agent at start

--- modules/software/ssh.nix
+++ modules/software/ssh.nix
@@ -3,5 +3,6 @@
     services.openssh.enable = true; # Enable the OpenSSH daemon.
     programs.ssh.enableAskPassword = false; # Disable Askpass (hopefully)
     programs.gnupg.agent.enableSSHSupport = true;
+    programs.ssh.startAgent = true;
 
 }
