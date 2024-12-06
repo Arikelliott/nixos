a7bcf6468b05056d658046fe027669d0d448d144
Arikelliott <20728146+Arikelliott@users.noreply.github.com>
2024-12-06 15:10:48 -0500

    Switched from GnuPG to ssh-agent

--- modules/software/ssh.nix
+++ modules/software/ssh.nix
@@ -2,7 +2,7 @@
 
     services.openssh.enable = true; # Enable the OpenSSH daemon.
     programs.ssh.enableAskPassword = false; # Disable Askpass (hopefully)
-    programs.gnupg.agent.enableSSHSupport = true;
+    # programs.gnupg.agent.enableSSHSupport = true;
     programs.ssh.startAgent = true;
 
 }
