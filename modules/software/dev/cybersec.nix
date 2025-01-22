e34f3f32c1123082d234d11c9507a2af764881a5
Arikelliott <20728146+Arikelliott@users.noreply.github.com>
2025-01-22 16:10:52 -0500

    added Cisco Packet Tracer

--- modules/software/dev/cybersec.nix
+++ modules/software/dev/cybersec.nix
@@ -4,6 +4,7 @@
 
 environment.systemPackages = with pkgs; [
     john
+    ciscoPacketTracer8
 ];
 
 }
--- modules/software/fun-cli-utils.nix
+++ modules/software/fun-cli-utils.nix
@@ -2,18 +2,23 @@
 
 {
 
-	environment.systemPackages = with pkgs; [
-	
+	environment.systemPackages = (with pkgs; [
+
 	feh # A light-weight image viewer.
     figlet # Generate ASCII word art.
     gallery-dl # Download images galleries from image hosting sites.
-    yt-dlp # Youtube/video downloader, youtube-dl fork.
     ytfzf # Find, download, and watch Youtube videos from the terminal.
     glow # Utility for rendering Markdown in the terminal.
     neofetch # System info fetcher and snowflake displayer.
-
     cool-retro-term # An overly-fancy terminal emulator.
 
-	];
+	])
+
+	++
+
+	(with pkgs.unstable; [
+		yt-dlp # Youtube/video downloader, youtube-dl fork.
+	]);
+
 
 }
