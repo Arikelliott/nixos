9d4ae9458350db18cd8ecf9d800bf524ed7ebe82
Arikelliott <20728146+Arikelliott@users.noreply.github.com>
2025-02-20 20:55:47 -0500

    Rearrange tmux nix file structure and add some extra options

--- home-manager/shell/default.nix
+++ home-manager/shell/default.nix
@@ -4,6 +4,6 @@
     imports = [
 	    ./zsh.nix # Activate zsh config
 		./zoxide.nix # Support for zoxide integration
-		./tmux.nix # tmux settings
+		./tmux # tmux settings
 	];
 }
---
+++ home-manager/shell/tmux/default.nix
@@ -0,0 +1,57 @@
+{ inputs, config, pkgs, ... }:
+
+{
+
+programs.tmux = {
+	enable = true;
+	mouse = true;
+	baseIndex = 1;
+	clock24 = false;
+	newSession = true;
+	prefix = "C-Space";
+	terminal = "screen-256color";
+
+	plugins = with pkgs; [
+		tmuxPlugins.cpu # Shows CPU usage in status bar
+		# tmuxPlugins.vim-tmux-navigator # Allows CTRL+hjkl for pane navigation, plus adds vim integration
+		# tmuxPlugins.vim-tmux-focus-events # vim integration
+		tmuxPlugins.tmux-fzf # Adds fzf integration
+		# tmuxPlugins.power-theme # Powerline-style theme, might make my own later
+
+
+		{
+			plugin = tmuxPlugins.resurrect;
+			extraConfig = "set -g @resurrect-strategy-nvim 'session'";
+		}
+		{
+			plugin = tmuxPlugins.continuum;
+			extraConfig = ''
+				set -g @continuum-restore 'on'
+				set -g @continuum-save-interval '60' # minutes
+			'';
+		}
+	];
+
+	extraConfig = ''
+		# Do shift + r to reload config
+		bind R source-file ~/.config/tmux/tmux.conf
+
+		# Bind ctrl + f for launching fuzzy finder
+		TMUX_FZF_LAUNCH_KEY="C-f"
+	'';
+
+};
+
+programs.fzf = {
+	enable = true;
+	tmux = {
+		enableShellIntegration = true;
+		shellIntegrationOptions = [ ];
+	};
+};
+
+# imports = [
+#	./tmux-theme.nix
+#];
+
+}
