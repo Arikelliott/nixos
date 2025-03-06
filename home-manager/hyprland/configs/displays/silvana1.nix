a592f5a60002d65d332bbb0cee16f6b550567c0e
Arikelliott <20728146+Arikelliott@users.noreply.github.com>
2025-03-06 13:10:21 -0500

    Trying out Hyprland with no persistent workspaces

--- home-manager/hyprland/configs/displays/silvana1.nix
+++ home-manager/hyprland/configs/displays/silvana1.nix
@@ -7,17 +7,30 @@
         };
 
         workspace = [
-            "1, persistent:true, default:true"
-            "2, persistent:true"
-            "3, persistent:true"
-            "4, persistent:true"
-            "5, persistent:true"
-            "6, persistent:true"
-            "7, persistent:true"
-            "8, persistent:true"
-            "9, persistent:true"
-            "10, persistent:true"
+            "1, default:true"
+            "2"
+            "3"
+            "4"
+            "5"
+            "6"
+            "7"
+            "8"
+            "9"
+            "10"
         ];
+
+        # workspace = [
+        #     "1, persistent:true, default:true"
+        #     "2, persistent:true"
+        #     "3, persistent:true"
+        #     "4, persistent:true"
+        #     "5, persistent:true"
+        #     "6, persistent:true"
+        #     "7, persistent:true"
+        #     "8, persistent:true"
+        #     "9, persistent:true"
+        #     "10, persistent:true"
+        # ];
     };
 
 
