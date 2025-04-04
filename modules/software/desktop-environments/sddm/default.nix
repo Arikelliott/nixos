576f07be1a2d9bde743a2ec9a88f3c4d892bef52
Arikelliott <20728146+Arikelliott@users.noreply.github.com>
2025-04-04 18:28:53 -0400

    Tweaked SDDM slightly, gonna hopefully get themes working someday

--- modules/software/desktop-environments/sddm/default.nix
+++ modules/software/desktop-environments/sddm/default.nix
@@ -8,11 +8,7 @@
         wayland.enable = true;
         autoNumlock = true;
 #        theme = "${pkgs.sddm-astronaut}/share/sddm/themes/sddm-astronaut-theme";
-#        theme = "sddm-astronaut-theme";
-        settings = {
-            Theme = {
-
-        };
+		theme = "sddm-astronaut";
     };
     services.displayManager.defaultSession = "hyprland";
 
@@ -25,3 +21,8 @@
     ];
 
 }
+
+# Resources for figuring out the stupid theme:
+# https://github.com/Zhaith-Izaliel/sddm-sugar-candy-nix
+# https://github.com/Zhaith-Izaliel/zhaith-nixos-configuration/blob/5a86b49763bc0be480dfbd5e6dda8d13ce61f57a/modules/system/display-manager.nix#L73
+# https://github.com/search?q=repo%3ASly-Harvey%2FNixOS%20sddm&type=code
