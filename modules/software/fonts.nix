aaacdbbe94901a0ac3fa485ea687262d2dfc4f39
Arikelliott <20728146+Arikelliott@users.noreply.github.com>
2025-04-27 18:35:56 -0400

    Fixed nerdfonts package

--- modules/software/fonts.nix
+++ modules/software/fonts.nix
@@ -2,23 +2,20 @@
 
 {
 
-fonts.packages = with pkgs; [
+	fonts.packages = with pkgs; [
 
-	noto-fonts # Google's universal font.
-	font-awesome # scalable vector icon font.
-	jetbrains-mono
-	corefonts # Microsoft's TTF core fonts for the web.
-	helvetica-neue-lt-std # Helvetica Neue Standard
-	aileron # Clean sans serif font, sorta similar to Helvetica
+		noto-fonts # Google's universal font.
+		font-awesome # scalable vector icon font.
+		jetbrains-mono
+		corefonts # Microsoft's TTF core fonts for the web.
+		helvetica-neue-lt-std # Helvetica Neue Standard
+		aileron # Clean sans serif font, sorta similar to Helvetica
 
-	materia-theme # Materia GTK theme.
-	material-icons # Google's Material icons.
-	papirus-icon-theme
+		materia-theme # Materia GTK theme.
+		material-icons # Google's Material icons.
+		papirus-icon-theme
+		nerdfonts
 
-]
-
-++
-
-builtins.filter lib.attrsets.isDerivation (builtins.attrValues pkgs.nerd-fonts); # Giant nerd font package.
+	];
 
 }
