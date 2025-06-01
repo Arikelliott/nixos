86d9a012ef45d6b82350fca391d7a940de334dff
Arikelliott <20728146+Arikelliott@users.noreply.github.com>
2025-06-01 18:30:46 -0400

    Removed regular nvim install so NixVim flake works properly

--- modules/software/productivity/text-editors.nix
+++ modules/software/productivity/text-editors.nix
@@ -5,7 +5,7 @@
 environment.systemPackages = with pkgs; [
 
 	vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-	neovim # Extensible vim fork.
+	# neovim # Extensible vim fork.
 	# vscodium # Fancy GUI IDE.
 	zed-editor # New IDE trying to replace vscodium.
 	jetbrains.idea-community-bin
