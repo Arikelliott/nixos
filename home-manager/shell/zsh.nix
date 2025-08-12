4c44d64e476b802b12a25c20642e86d40babbde0
Arikelliott <20728146+Arikelliott@users.noreply.github.com>
2025-08-11 21:23:41 -0400

    Replaced powerlevel10k with starship

--- home-manager/shell/zsh.nix
+++ home-manager/shell/zsh.nix
@@ -41,19 +41,7 @@
 
 	# Commands to add into .zshrc
 	initContent = ''
-		# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-		# Initialization code that may require console input (password prompts, [y/n]
-		# confirmations, etc.) must go above this block; everything else may go below.
-		if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
-		source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
-		fi
-		# --- Enable p10k theme
-		source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
-		# --- To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-		# ~/.p10k.zsh is the where the powerlevel10k config you initially set is stored
-		[[ ! -f $ZDOTDIR/.p10k.zsh ]] || source $ZDOTDIR/.p10k.zsh
-
-
+		eval "$(starship init zsh)"
 
 			# if command -v tmux &> /dev/null && [ -n "$PS1" ] && [[ ! "$TERM" =~ screen ]] && [[ ! "$TERM" =~ tmux ]] && [ -z "$TMUX" ]; then
 		#	exec tmux attach
--- modules/software/zsh.nix
+++ modules/software/zsh.nix
@@ -5,9 +5,10 @@
 	programs.zsh.enable = true; # Enable zsh.
 	users.defaultUserShell = pkgs.zsh; # Set zsh as default shell.
 	environment.pathsToLink = [ "/share/zsh" ];
+	programs.starship.enable = true;
 
 	environment.systemPackages = [
-	pkgs.zsh-powerlevel10k
+		pkgs.starship
 	];
 
 }
