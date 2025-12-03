f9a51b6db0d0531a2e197bbf40a4b6327950d8d0
Arikelliott <20728146+Arikelliott@users.noreply.github.com>
2025-12-02 20:08:30 -0500

    Moved settings to single file

--- modules/settings/default.nix
+++ modules/settings/default.nix
@@ -20,6 +20,22 @@
 		};
 	};
 
+
+
+# - NETWORKING
+
+	networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain"; # Configure network proxy if necessary
+	# - Firewall -
+	# Open ports in the firewall.
+	# networking.firewall.allowedTCPPorts = [ ... ];
+	# networking.firewall.allowedUDPPorts = [ ... ];
+	# Or disable the firewall altogether.
+	networking.firewall.enable = false;
+
+
+
+# - PACKAGE STUFF
+
 # - Enable support for unpatched dynamic binaries on NixOS -
 	programs.nix-ld.enable = true;
 
