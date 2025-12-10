e06fff556b620500251bbb6addd6258382c211f7
Arikelliott <20728146+Arikelliott@users.noreply.github.com>
2025-12-09 20:13:37 -0500

    Fixed Anyara uid

--- modules/users/user-anyara.nix
+++ modules/users/user-anyara.nix
@@ -4,7 +4,7 @@ users.users.anyara = {
 	isNormalUser = true;
 	initialPassword = "pw123";
 	extraGroups = [ "wheel" "cdrom" "wireshark" ]; # Enable ‘sudo’ for the user using "wheel" group.
-	uid=1000; # Set User ID
+	uid=1003; # Set User ID
 };
 
 nix.settings.trusted-users = [
