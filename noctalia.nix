d912173538604471cd2bc92510394e7cc19a57a1
Arikelliott <20728146+Arikelliott@users.noreply.github.com>
2026-04-11 15:28:47 -0400

    Fixed formatting slightly in noctalia.nix

--- noctalia.nix
+++ noctalia.nix
@@ -1,8 +1,8 @@
 { pkgs, inputs, ... }:
 {
-  # install package
-  environment.systemPackages = with pkgs; [
-    inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
-    # ... maybe other stuff
-  ];
+	# install package
+  	environment.systemPackages = with pkgs; [
+		inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
+  	  	# ... maybe other stuff
+  	];
 }
