4ec5a5adaac0f6d9ba62bcbb9488696e1ef37aaa
Arikelliott <20728146+Arikelliott@users.noreply.github.com>
2026-04-12 19:01:42 -0400

    Cleaned up the theme file stuff

--- modules/themes.nix
+++ modules/themes.nix
@@ -3,7 +3,13 @@
 {
 
 	environment.systemPackages = with pkgs; [
+		# GTK themes
 		adw-gtk3
+		# Icon themes
+		whitesur-icon-theme
+		papirus-folders
+		papirus-icon-theme
+		papirus-maia-icon-theme
 	];
 
 }
