11489516e6a69ca64e03460a5e30a29b86b6c1b8
Arikelliott <20728146+Arikelliott@users.noreply.github.com>
2026-06-14 21:30:56 -0400

    added nh

--- modules/settings.nix
+++ modules/settings.nix
@@ -16,10 +16,17 @@
 			warn-dirty = false;
 		};
 		settings.auto-optimise-store = true;
-		gc = {
-			automatic = true;
-			dates = "weekly";
-		};
+		# gc = {
+		# 	automatic = true;
+		# 	dates = "weekly";
+		# };
+	};
+
+	programs.nh = {
+		enable = true;
+		clean.enable = true;
+		clean.extraArgs = "--keep-since 4d --keep 3";
+		# flake = "/home/arik/nixos";
 	};
 
 
