61d2771cbc0bc65eaf17fe4c1b032e031e5022a0
Arikelliott <20728146+Arikelliott@users.noreply.github.com>
2026-06-16 20:27:26 -0400

    Added thingy to hopefully make Steam start right

--- hosts/silvana/hardware-configuration.nix
+++ hosts/silvana/hardware-configuration.nix
@@ -35,4 +35,8 @@
 
 	nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 	hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+	hardware.graphics.enable = true;
+	hardware.graphics.enable32Bit = true;
+
 }
